3f73c14d31ec587a9cc723115e6fe324c3506036
Johannes Rieke <johannes.rieke@gmail.com>
2024-06-26 23:54:44 +0200

    Update description

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from openai import OpenAI
 st.title("💬 Chatbot")
 st.write(
     "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use it, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
+    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
     "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
 )
 
